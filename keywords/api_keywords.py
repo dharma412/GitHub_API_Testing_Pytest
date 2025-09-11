7be4143e411ecb26b127cc517d511868ddc585ea
userteja <YoueNamw@gmail.com>
2025-09-11 09:52:03 +0530

    Created yaml file testing.

--- keywords/api_keywords.py
+++ keywords/api_keywords.py
@@ -1,19 +1,21 @@
 import os
 import json
 
-def load_repo_data():
-    with open('../Data/test_data.json') as f:
+def load_repo_data(filepath):
+    with open(filepath) as f:
         return json.load(f)
 
 def fetch_repo(github_session):
     response = github_session.get(os.getenv('BASE_URL')+'/users/dharma412/repos',verify=False)
+    if response.text:
+        print(response.text)
     return  response
 
-def create_repo(github_session, repo_data=load_repo_data()):
+def create_repo(github_session, repo_data=load_repo_data('../Data/test_data.json')):
     response = github_session.post(os.getenv('BASE_URL')+'/user/repos', json=repo_data,verify=False)
     return response
 
-def update_repo(github_session, owner, repo, update_data):
-    url = f"{github_session.base_url}/repos/{owner}/{repo}"
-    response = github_session.patch(url, json=update_data)
+def update_repo(github_session,old_repo, update_data=load_repo_data('../Data/update_repo_data.json')):
+    url = f"{os.getenv('BASE_URL')}/repos/dharma412/{old_repo}"
+    response = github_session.patch(url, json=update_data,verify=False)
     return response
--- tests/test_repositories.py
+++ tests/test_repositories.py
@@ -9,24 +9,10 @@ def test_fetch_user_repo_data(github_session):
 
 @pytest.mark.git2
 def test_create_repo(github_session):
-    # repo_data = {
-    #     "name": "test-repo11",
-    #     "description": "Created via API",
-    #     "private": False
-    # }
     response = create_repo(github_session)
     assert response.status_code == 201
 
 
-# def test_update_repo(github_session):
-#     owner = "dharma412"
-#     repo = "test-repo"
-#     update_data = {
-#         "description": "Updated description"
-#     }
-#     response = update_repo(github_session, owner, repo, update_data)
-#     assert response.status_code == 200
-
-
-# def test_delete_repo():
-#     pass
+def test_update_repo(github_session):
+    response = update_repo(github_session, 'test-teja')
+    assert response.status_code == 200
