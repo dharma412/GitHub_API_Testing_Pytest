c377bed5fd75d10ba42d3453b264d74eda63207d
userteja <YoueNamw@gmail.com>
2025-09-11 15:01:33 +0530

    Created yaml file testing.

--- tests/test_repositories.py
+++ tests/test_repositories.py
@@ -10,9 +10,10 @@ def test_fetch_user_repo_data(github_session):
 @pytest.mark.git2
 def test_create_repo(github_session):
     response = create_repo(github_session)
+    print(response.status_code)
     assert response.status_code == 201
 
 
-def test_update_repo(github_session):
-    response = update_repo(github_session, 'test-teja')
-    assert response.status_code == 200
+# def test_update_repo(github_session):
+#     response = update_repo(github_session, 'test-teja')
+#     assert response.status_code == 200
