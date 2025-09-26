96223d1214781e810f9b10d23662d6b2066aee5d
userteja <YoueNamw@gmail.com>
2025-09-26 21:20:08 +0530

    fixed issue

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,14 @@
 import os
 import requests
 import pytest
-
+import json
+from pygments.lexer import default
 
 def pytest_addoption(parser):
     parser.addoption("--url", default=os.getenv('BASE_URL', 'https://api.github.com'))
     parser.addoption("--token", default=None)
-
-
+    parser.addoption("--username",default=None)
+    parser.addoption("--user_name", default="dharma412")
 
 @pytest.fixture(scope='session')
 def github_session(request):
@@ -19,9 +20,18 @@ def github_session(request):
     session = requests.Session()
     session.headers.update(headers)
     yield session
+    print("end of testcase")
     session.close()
+    clear_repo_data()
 
+def clear_repo_data():
+    with open('../Data/repo_data.json', 'w') as f:
+        json.dump({"repo_name": []}, f)
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope='session')
 def base_url(request):
     return request.config.getoption("--url")
+
+@pytest.fixture(scope='session')
+def username(request):
+    return request.config.getoption("--user_name")
--- tests/test_repositories.py
+++ tests/test_repositories.py
@@ -3,8 +3,8 @@ from  keywords.api_keywords import *
 
 
 @pytest.mark.git
-def test_fetch_user_repo_data(github_session, base_url):
-    response = fetch_repo(github_session, base_url)
+def test_fetch_user_repo_data(github_session, base_url,username):
+    response = fetch_repo(github_session, base_url,username)
     assert response.status_code==200
 
 @pytest.mark.git2
@@ -14,15 +14,16 @@ def test_create_repo(github_session,base_url):
     assert response.status_code == 201
 
 @pytest.mark.git2
-def test_update_repo(github_session,base_url):
-    response = update_repo(github_session,base_url)
+def test_update_repo(github_session,base_url,username):
+    response = update_repo(github_session,base_url,username)
     assert response.status_code == 200
-
+#
 @pytest.mark.git2
-def test_delete_repo(github_session,base_url):
-    response = delete_repo(github_session,base_url)
+def test_delete_repo(github_session,base_url,username):
+    response = delete_repo(github_session,base_url,username)
     assert response.status_code==204
 
+# create repo -- repo--> repo_data.py update
 
 
 
