6f63dc05c57095749374a2bab61e0fc4d1119746
Your Name <>
2026-02-13 20:50:50 +0530

    ci updated

--- tests/test_branches.py
+++ tests/test_branches.py
@@ -3,19 +3,16 @@ from keywords.api_keywords import create_repo, create_empty_commit
 
 
 @pytest.mark.branch
-@pytest.mark.dependency(name="create_repo")
 def test_create_repo(github_session,base_url,context):
     response = create_repo(github_session,base_url,context)
     print(response.status_code)
     assert response.status_code == 201
 
 @pytest.mark.branch
-@pytest.mark.dependency(depends=["create_repo"])
 def test_commit_on_repo(github_session,base_url,username,context):
     responses= create_empty_commit(github_session,base_url,username,context)
     assert responses.status_code == 201
 
 @pytest.mark.branch
-@pytest.mark.dependency(depends=["test_create_repo"])
 def create_branch(github_session,base_url):
     pass
--- tests/test_repositories.py
+++ tests/test_repositories.py
@@ -13,11 +13,13 @@ def test_create_repo(github_session,base_url,context):
     print(response.status_code)
     assert response.status_code == 201
 
+@pytest.mark.repo
 @pytest.mark.git2
 def test_update_repo(github_session,base_url,username):
     response = update_repo(github_session,base_url,username)
     assert response.status_code == 200
 
+@pytest.mark.repo
 @pytest.mark.git2
 def test_delete_repo(github_session,base_url,username):
     response = delete_repo(github_session,base_url,username)
